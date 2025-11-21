2319be3bc76776a087ee45ba91bd9a3e6263f8d0
JIN <jin724596@gmail.com>
2025-11-22 00:50:10 +0900

    loginViewController 추가

--- assginment03/assginment03/Extension/UITextField+.swift
+++ assginment03/assginment03/Extension/UITextField+.swift
@@ -10,32 +10,46 @@ import UIKit
 
 extension UITextField {
     
-    
-    func addHorizontalPadding() {
+    func addPadding(left: CGFloat = 0, right: CGFloat = 0) {
+        if left > 0 {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: self.frame.height))
+            self.leftView = paddingView
+            self.leftViewMode = .always
+        }
         
-        let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
-        self.leftView = leftPadding
-        self.leftViewMode = ViewMode.always
         
-        let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
-        self.rightView = rightPadding
-        self.rightViewMode = ViewMode.always
-        
-    }
-    
-    func addLeftPadding() {
-        
-        let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
-        self.leftView = leftPadding
-        self.leftViewMode = ViewMode.always
+        if right > 0 {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: self.frame.height))
+            self.rightView = paddingView
+            self.rightViewMode = .always
+        }
         
     }
-    
-    func addRightPadding() {
+        func addHorizontalPadding() {
+            
+            let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
+            self.leftView = leftPadding
+            self.leftViewMode = ViewMode.always
+            
+            let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
+            self.rightView = rightPadding
+            self.rightViewMode = ViewMode.always
+            
+        }
         
-        let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
-        self.rightView = rightPadding
-        self.rightViewMode = ViewMode.always
-    }
+        func addLeftPadding() {
+            
+            let leftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
+            self.leftView = leftPadding
+            self.leftViewMode = ViewMode.always
+            
+        }
+        
+        func addRightPadding() {
+            
+            let rightPadding = UIView(frame: CGRect(x: 0, y: 0, width: 16,height: self.frame.height))
+            self.rightView = rightPadding
+            self.rightViewMode = ViewMode.always
+        }
     
 }
